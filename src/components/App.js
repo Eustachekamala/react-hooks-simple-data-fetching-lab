bc668541ede2ab17c122ec212844c3c05c280bb5
eustachekamala <eustachejared25@gmail.com>
2024-08-20 13:41:02 +0300

    I used the fetch to take the values on the API and then render it

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import { useState, useEffect } from "react";
+
+
+function App (){
+    const[imageUrl, setImageUrl] = useState('');
+    const[isLoaded, setIsLoaded] = useState(true);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(reponse => reponse.json())
+        .then(data => {
+            setImageUrl(data.message);
+            setIsLoaded(false);
+        })
+}, [])
+
+    return (
+        <div>
+            {isLoaded ? (<p>Loading ...</p>) : (
+                <img src={imageUrl} alt="A Random Dog" />)
+            }
+        </div>
+    )
+}
+
+export default App
